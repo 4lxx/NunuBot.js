d5c2214e0103a26d4ddc338727c1a27aad967083
OpenShift guest <burntime@live.de>
2014-02-27 08:48:00 -0500

    connect to teamspeak and get clients, resteemo stub, colors for console output

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-var config = require('./config.json');
+var config    = require('./config.json'),
+    teamspeak = require('./lib/teamspeak'),
+    log       = require('./lib/console_out');
+
+var lolUser = [];
+
+teamspeak.connect(function(){
+  teamspeak.getClientList(function(clients) {
+    filterClientList(clients);
+  });
+});
+
+function filterClientList(clients) {
+ clients.forEach(function(client) {
+   config.teamspeak.lolChans.forEach(function(chanID) {
+    if (chanID === client.cid) {
+      lolUser.push(client);
+      teamspeak.getClientGroupList(client, function(response) {
+        console.log(response);
+      });
+    }
+   });
+ }); 
+}
